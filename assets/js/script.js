788dc4341d09377fed40eee36e7b6fa167903468
Durgesh <nagarkardurgesh@gmail.com>
2021-01-09 23:30:17 +0530

    css and js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 /* Author: Durgesh
 
 */
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
 
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+})
 
 
 
